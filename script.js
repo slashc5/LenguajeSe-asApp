7725f086fc1f1fee086c05f545861230dfeaaae4
Benjamin Axel <axel_313@yahoo.com>
2022-07-30 11:37:13 -0300

    cambios en la aplicacion

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 let seña
 
+let = nombreUsuario = prompt("Ingrese su nombre").toLowerCase()
+let = apellidoUsuario = prompt("Ingrese su apellido").toLowerCase()
+let = edadUsuario = parseInt(prompt("Ingrese su edad"))
+let = alturaUsuario = parseFloat(prompt("Ingrese su altura"))
+
+class Usuario {
+    constructor(nombre, apellido, edad, altura) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+        this.altura = altura
+    }
+
+    aumentarEdad(porcAumento) {
+        this.edad += porcAumento
+    }
+
+
+    disminuirEdad(porcDisminucion) {
+        this.edad -= porcDisminucion
+    }
+
+    aumentarAltura(nuevaAltura) {
+        this.altura += nuevaAltura
+    }
+
+    disminuirAltura(bajarAltura) {
+        this.altura -= bajarAltura
+    }
+
+
+}
+const usuario0 = new Usuario(nombreUsuario, apellidoUsuario, edadUsuario, alturaUsuario)
+const usuario1 = new Usuario("Sol", "Nievas", 19, 1.57)
+const usuario2 = new Usuario("Lucas", "Mauricio", 27, 1.67)
+
+const usuarios = [usuario1, usuario2, usuario0]
 function menuSeña() {
     alert("En este menu se van a mostrar señas que podrias usar para comunicarte!\n\n👋🏼 - Hola, chau \n✋🏼🤛🏼 - Chocar\n ✋🏼👈🏼 - ¿Qué?\n 🤷🏽‍♀️ - No sé")
 }
@@ -16,8 +53,10 @@ function personalizadoPalabra() {
 function cambiarLenguaje() {
     alert("Por el momento esto se encuentra en mantenimiento")
 }
+alert(nombreUsuario, "bienvenido")
+alert("Te damos la Bienvenida")
 do {
-    seña = prompt("Bienvenidos al Proyecto ZEIT por favor ingresa una opcion:\n\n1 - Lenguaje de señas\n2 - Numeros Personalizados(36)\n3 - Otro lenguaje\n4 - Salir de la aplicacion")
+    seña = prompt("Bienvenidos al Proyecto ZEIT por favor ingresa una opcion:\n\n1 - Lenguaje de señas\n2 - Numeros Personalizados(36)\n3 - Otro lenguaje\n4 - Salir de la aplicacion - \n5 - Mi perfil - \n6 - Lista de usuarios")
     switch (seña) {
         case '1':
             menuSeña();
@@ -31,8 +70,17 @@ do {
         case '4':
             console.log("Nos vemos la proxima! :D");
             break;
+        case '5':
+            alert("A continuacion en la consola se mostrara tu perfil de usuario")
+            console.log(usuario0)
+            break;
+        case '6':
+            alert("A continuacion te mostraremos la lista de usuarios registrados")
+            console.log(usuarios)
+            break;
         default:
             console.log("No reconocimos ese comando :c")
             break;
     }
-} while (seña != '4')
+} while (seña != '6')
+
